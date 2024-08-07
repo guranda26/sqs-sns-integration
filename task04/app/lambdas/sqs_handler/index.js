1f1cc9723e4fc2bba45734fbc5f98f85eae64f5f
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-08 02:37:58 +0400

    update credentials

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
-  const response = {
+  console.log("SQS Message:", event.Records[0].body);
+  return {
     statusCode: 200,
-    message: "Hello from Lambda!",
+    body: JSON.stringify({ message: "SQS message processed successfully" }),
   };
-  return response;
 };
