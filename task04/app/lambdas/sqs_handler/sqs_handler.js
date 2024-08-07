e169a15af9948c310bcbf01044974a4693880d03
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-08 03:08:16 +0400

    create sqs_handler.js file

--- task04/app/lambdas/sqs_handler/sqs_handler.js
+++ task04/app/lambdas/sqs_handler/sqs_handler.js
@@ -1,5 +1,8 @@
 exports.handler = async (event) => {
-  console.log("SQS Message:", event.Records[0].body);
+  console.log("SQS event:", JSON.stringify(event, null, 2));
+  const messageBody = event.Records[0].body;
+  console.log("Message body:", messageBody);
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "SQS message processed successfully" }),
