6e1c190ef3512e45940de5bcca35e008d9a26415
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-08 03:01:00 +0400

    modify handler function

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,7 +1,25 @@
+const AWS = require("aws-sdk");
+
 exports.handler = async (event) => {
-  console.log("SQS Message:", event.Records[0].body);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "SQS message processed successfully" }),
+  const sqs = new AWS.SQS();
+  const params = {
+    QueueUrl:
+      "https://sqs.eu-central-1.amazonaws.com/196241772369/cmtr-60b8d96c-async_queue",
+    MessageBody: "Test message",
   };
+
+  try {
+    const data = await sqs.sendMessage(params).promise();
+    console.log("SQS Message Sent:", data.MessageId);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "SQS message sent successfully" }),
+    };
+  } catch (error) {
+    console.error("Error sending SQS message:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to send SQS message" }),
+    };
+  }
 };
