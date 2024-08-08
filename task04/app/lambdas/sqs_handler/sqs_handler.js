c1f386971dd74d6e9f815e62e68665115c2c4f58
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-08 19:18:21 +0400

    update resources

--- task04/app/lambdas/sqs_handler/sqs_handler.js
+++ task04/app/lambdas/sqs_handler/sqs_handler.js
@@ -1,12 +1,7 @@
 exports.handler = async (event) => {
   console.log("SQS event:", JSON.stringify(event, null, 2));
   const messageBody = event.Records[0].body;
-  console.log("Message body:", messageBody);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "SQS message processed successfully" }),
-  };
+  return `Successfully processed ${event.Records.length} messages.`;
 };
 // @SqsTriggerEventSource(targetQueue = 'async_queue', batchSize=10)
 // @LambdaHandler(lambdaName = 'sqs_handler', roleName='sqs_handler-role', isPublichVersion = false, logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
