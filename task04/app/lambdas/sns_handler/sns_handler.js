e319a6ed31ff3f4c9bd7eaf096c7a0c7dbe88619
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-09 00:00:18 +0400

    remove last commit

--- task04/app/lambdas/sns_handler/sns_handler.js
+++ task04/app/lambdas/sns_handler/sns_handler.js
@@ -1,23 +1,12 @@
 const AWS = require("aws-sdk");
 
 exports.handler = async (event) => {
-  try {
-    console.log("SNS Event:", JSON.stringify(event, null, 2));
+  console.log("SNS Event:", JSON.stringify(event));
+  const message = event.Records[0].Sns.Message;
+  console.log("SNS Message:", message);
 
-    event.Records.forEach((record) => {
-      const snsMessage = record.Sns.Message;
-      console.log("SNS Message:", snsMessage);
-    });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "SNS message processed successfully" }),
-    };
-  } catch (error) {
-    console.error("Error processing SNS message:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Error processing SNS message" }),
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "SNS message processed successfully" }),
+  };
 };
--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,4 +1,5 @@
 exports.handler = async (event) => {
+  console.log("SQS Message:", event.Records[0].body);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "SQS message processed successfully" }),
