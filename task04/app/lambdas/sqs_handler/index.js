4a5ec5a829e97105ed32cc5def475da498026b82
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-12 16:06:02 +0400

    update index

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
   console.log("SQS Message:", event.Records[0].body);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "SQS message processed successfully" }),
+    body: JSON.stringify("SQS message processed successfully"),
   };
 };
